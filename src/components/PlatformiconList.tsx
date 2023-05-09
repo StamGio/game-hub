8214446cb51fde043130f6917f7f451533e71c3d
StamGio <stamatisgio@icloud.com>
2023-05-09 14:43:04 +0300

    Platform icons

---
+++ src/components/PlatformiconList.tsx
@@ -0,0 +1,45 @@
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+export const PlatformiconList = ({ platforms }: Props) => {
+  // index signature
+  const iconMap: { [key: string]: IconType } = {
+    // name : PlayStation
+    // slug : playstation // better count on slug
+
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+  };
+
+  return (
+    // HStack for Horizontal stack
+    <HStack marginY={"10px"}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
