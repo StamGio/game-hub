8ce7067a5bd592f317dff6f40bbdfd63fc4aa061
StamGio <stamatisgio@icloud.com>
2023-05-17 00:14:31 +0300

    Add Spinner

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,9 +1,11 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenresList = () => {
-  const { data } = useGenres();
+  const { data, loading, error } = useGenres();
+  if (error) return null;
+  if (loading) return <Spinner />;
   return (
     <List>
       {data.map((genre) => (
