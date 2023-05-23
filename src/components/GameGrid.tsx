d0421600ccb0de835d35ed435a531fdb1d296474
StamGio <stamatisgio@icloud.com>
2023-05-23 14:09:34 +0300

    Refactor GameGrid error

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,23 +14,22 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, loading } = useGames(gameQuery);
   const Skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  if (error) return <Text color="#E53E3E">{error}</Text>;
+
   return (
-    <>
-      {error && <Text color="#E53E3E">{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
-        {loading &&
-          Skeletons.map((skeleton) => (
-            <GameCardComponent key={skeleton}>
-              <GameCardSceleton />
-            </GameCardComponent>
-          ))}
-        {data.map((game) => (
-          <GameCardComponent key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+      {loading &&
+        Skeletons.map((skeleton) => (
+          <GameCardComponent key={skeleton}>
+            <GameCardSceleton />
           </GameCardComponent>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardComponent key={game.id}>
+          <GameCard game={game} />
+        </GameCardComponent>
+      ))}
+    </SimpleGrid>
   );
 };
 
